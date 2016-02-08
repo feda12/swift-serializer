5730a599f946afb40f25c515ba1569a2b3b13e8b
Ben Le Cam <blecam@four51.com>
2016-02-08 11:34:59 -0600

    Added targets for watchOS and tvOS

---
+++ SwiftSerializerWatchOS/SwiftSerializerWatchOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSerializerWatchOS.h
+//  SwiftSerializerWatchOS
+//
+//  Created by Ben Le Cam on 2/8/16.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SwiftSerializerWatchOS.
+FOUNDATION_EXPORT double SwiftSerializerWatchOSVersionNumber;
+
+//! Project version string for SwiftSerializerWatchOS.
+FOUNDATION_EXPORT const unsigned char SwiftSerializerWatchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSerializerWatchOS/PublicHeader.h>
+
+
---
+++ SwiftSerializertvOS/SwiftSerializertvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSerializertvOS.h
+//  SwiftSerializertvOS
+//
+//  Created by Ben Le Cam on 2/8/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSerializertvOS.
+FOUNDATION_EXPORT double SwiftSerializertvOSVersionNumber;
+
+//! Project version string for SwiftSerializertvOS.
+FOUNDATION_EXPORT const unsigned char SwiftSerializertvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSerializertvOS/PublicHeader.h>
+
+
